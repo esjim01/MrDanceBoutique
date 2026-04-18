47371880056d272b32042f9763eb2a2cbd2d3864
esjim01 <eduar.jimenez45164@ucaldas.edu.co>
2026-04-18 16:35:20 -0500

    Se agrega puerto

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,6 +44,6 @@ app.get('/api/health', (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;//
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',  () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
